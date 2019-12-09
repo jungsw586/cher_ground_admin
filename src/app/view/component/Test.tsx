d1531496f94ab8519623da72307963cd4829a39d
jisuuu-yoon <55950322+jisuuu-yoon@users.noreply.github.com>
2019-12-09 15:57:56 +0900

    Delete Test.tsx

--- src/app/view/component/Test.tsx
+++
@@ -1,5 +0,0 @@
-// import React from "react";
-// import styled from "styled-components";
-// import WebPagenation from "../widget/WebPagenation";
-
-// <WebPagenation offsetNumber={0} limitNumber={10}></WebPagenation>;
