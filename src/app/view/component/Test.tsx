0446185b432b0a078341986c2d1d483114f964d3
Jisu <dbswltn111@gmail.com>
2019-12-09 15:54:26 +0900

    pagenation component done

---
+++ src/app/view/component/Test.tsx
@@ -0,0 +1,5 @@
+// import React from "react";
+// import styled from "styled-components";
+// import WebPagenation from "../widget/WebPagenation";
+
+// <WebPagenation offsetNumber={0} limitNumber={10}></WebPagenation>;
