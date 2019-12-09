087b0fc482b437e238bc7abf63393499bd1228d7
jisuuu-yoon <55950322+jisuuu-yoon@users.noreply.github.com>
2019-12-09 15:57:24 +0900

    Update BuildingView.tsx

--- src/app/view/component/BuildingView.tsx
+++ src/app/view/component/BuildingView.tsx
@@ -6,7 +6,6 @@ import MainBodyLayout from "../widget/MainBodyLayout";
 import MainContentLayout from "../widget/MainContentLayout";
 import PageTitle from "../widget/PageBarTitle";
 import SearchBar from "../widget/SearchBar";
-import WebPagenation from "../widget/WebPagenation";
 
 const BuildingView: React.FunctionComponent = () => {
   return (
@@ -16,7 +15,6 @@ const BuildingView: React.FunctionComponent = () => {
         <MenuBar />
         <MainContentLayout>
           <PageTitle title={"상가"} />
-          <WebPagenation offsetNumber={1} limitNumber={10} totalPage={200} />
           <SearchBar category={"상가"} />
           <div>상가페이지입니다.</div>
         </MainContentLayout>
